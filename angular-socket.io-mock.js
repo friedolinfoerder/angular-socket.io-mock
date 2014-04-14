c73b65dbadb9bc9473ccfed98a415a650ee462bc
friedolinfoerder <friedolinfoerder@gmx.de>
2014-04-14 18:05:31 +0200

    added forward call

--- angular-socket.io-mock.js
+++ angular-socket.io-mock.js
@@ -1,39 +1,78 @@
 /* global angular: false */
-var ng = angular.module("btford.socket-io",[])
-ng.provider("socket",function(){
-  this.$get = function($rootScope){
+var ng = angular.module("btford.socket-io",[]);
+ng.provider("socketFactory",function() {
+  this.$get = function($rootScope) {
+    
+    return function socketFactory(options) {
 
-    var obj = {}
-    obj.events = {}
-    obj.emits = {}
-
-    // intercept 'on' calls and capture the callbacks
-    obj.on = function(eventName, callback){
-      if(!this.events[eventName]) this.events[eventName] = []
-      this.events[eventName].push(callback)
-    }
-
-    // intercept 'emit' calls from the client and record them to assert against in the test
-    obj.emit = function(eventName){
-      var args = Array.prototype.slice.call(arguments,1)
-
-      if(!this.emits[eventName])
-        this.emits[eventName] = []
-      this.emits[eventName].push(args)
-    }
-
-    //simulate an inbound message to the socket from the server (only called from the test)
-    obj.receive = function(eventName){
-      var args = Array.prototype.slice.call(arguments,1)
-
-      if(this.events[eventName]){
-        angular.forEach(this.events[eventName], function(callback){
-          $rootScope.$apply(function() {
-            callback.apply(this, args)
-          })
-        })
-      }
-    }
-    return obj
-  }
-})
+      var obj = {}
+      obj.events = {}
+      obj.emits = {}
+      
+      var prefix = options.prefix || "socket:";
+  
+      // intercept 'on' calls and capture the callbacks
+      obj.on = function(eventName, callback) {
+        var events = this.events[eventName];
+        if(!events) {
+          events = this.events[eventName] = [];
+        }
+        events.push(callback);
+      };
+  
+      // intercept 'emit' calls from the client and record them to assert against in the test
+      obj.emit = function(eventName) {
+        var args = Array.prototype.slice.call(arguments,1);
+  
+        if(!this.emits[eventName]) {
+          this.emits[eventName] = [];
+        }
+        this.emits[eventName].push(args);
+      };
+  
+      //simulate an inbound message to the socket from the server (only called from the test)
+      obj.receive = function(eventName) {
+        var args = Array.prototype.slice.call(arguments,1);
+        
+        var events = this.events[eventName];
+        if(events) {
+          angular.forEach(events, function(callback) {
+            $rootScope.$apply(function() {
+              callback.apply(this, args);
+            });
+          });
+        }
+      };
+      
+      //simulate forward call
+      obj.forward = function (events, scope) {
+        if(events instanceof Array === false) {
+          events = [events];
+        }
+        if(!scope) {
+          scope = defaultScope;
+        }
+        events.forEach(function(eventName) {
+          var prefixedEvent = prefix + eventName;
+          var forwardBroadcast = function() {
+            scope.$broadcast(prefixedEvent)
+          });
+          scope.$on('$destroy', function () {
+            var objEvents = obj.events[eventName];
+            if(objEvents) {
+              angular.forEach(objEvents, function(fn, i) {
+                if(fn === forwardBroadcast) {
+                  objEvents.splice(i, 1)
+                  return;
+                }
+              });
+            }
+          });
+          obj.on(eventName, forwardBroadcast);
+        });
+      };
+      
+      return obj;
+    };
+  };
+});
